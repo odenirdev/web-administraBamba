4117827a57fc2a01929421dfcd0dc5cc9379cdcf
Odenir <odenirdev@gmail.com>
2020-10-07 18:47:54 -0300

    set url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:1337",
+    baseURL: "https://server.adsamba.tk",
     headers: {
         Authorization: localStorage.getItem("token"),
     },
